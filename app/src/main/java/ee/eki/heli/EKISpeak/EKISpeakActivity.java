547fb40bbc662aa3c53dbdb49656844352056eb6
Kaarel Kaljurand <kaljurand@gmail.com>
2015-03-29 00:47:57 +0100

    Some cleanup

--- app/src/main/java/ee/eki/heli/EKISpeak/EKISpeakActivity.java
+++ app/src/main/java/ee/eki/heli/EKISpeak/EKISpeakActivity.java
@@ -1,78 +1,155 @@
 package ee.eki.heli.EKISpeak;
 
-//http://www.androidhive.info/2012/01/android-text-to-speech-tutorial/
-
-import java.util.Locale;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
+import android.speech.tts.UtteranceProgressListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class EKISpeakActivity extends Activity implements TextToSpeech.OnInitListener {
-	
-	private TextToSpeech tts;
-    private Button btnSpeak;
-    private EditText txtText;
-    
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-        btnSpeak = (Button) findViewById(R.id.SpeakButton);
-        
-        txtText = (EditText) findViewById(R.id.speakText);
-		
-        //tts = new TextToSpeech(this, this);
-	}
+import java.util.HashMap;
+import java.util.Locale;
 
-	public void onResume (){
-		super.onResume(); 
-		tts = new TextToSpeech(this, this);
-	}
-	
-	public void onSettingsClicked(View v) {
-		Intent intent = new Intent();
-		intent.setAction("com.android.settings.TTS_SETTINGS");
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		this.startActivity(intent);
-	}
+/**
+ * Demo activity. This is a simple activity, for more serious testing install [TODO].
+ */
+public class EKISpeakActivity extends Activity {
 
-	
-	public void onSpeakClicked(View v) {
-		speakOut();
-	}
+    private static final String UTT_COMPLETED_FEEDBACK = "UTT_COMPLETED_FEEDBACK";
 
-    private void speakOut() {
-        String text = txtText.getText().toString();
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-    }
-	
-	
-	@Override
-	public void onInit(int status) {
-		 //peale keeleseadete muutmist tuleks uuesti kontrollida.
-		
-        if (status == TextToSpeech.SUCCESS) {
-             int result = tts.setLanguage(new Locale("est","EST"));
-             if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "This Language is not supported");
-                btnSpeak.setEnabled(false);
-            } else {
-                btnSpeak.setEnabled(true);
-                //speakOut();
+    private TextToSpeech mTts;
+    private TextView mTvMessages;
+    private EditText mText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        mText = (EditText) findViewById(R.id.etText);
+        mTvMessages = (TextView) findViewById(R.id.tvMessages);
+
+        ((Button) findViewById(R.id.bStart)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                say(mText.getText().toString());
             }
-         } else {
-            Log.e("TTS", "Initilization Failed!");
-        }
-		
-	}
-	
+        });
 
+        ((Button) findViewById(R.id.bStop)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+
+        ((Button) findViewById(R.id.bSettings)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("com.android.settings.TTS_SETTINGS");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        shutdown();
+        mTvMessages.setText("");
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mTts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                // TODO: print out available languages, and enable the buttons
+                log(mTts.getLanguage().toString());
+                String localeAsString = "et-EE";
+                log(localeAsString + " is available? " + isLanguageAvailable(localeAsString));
+                setLanguage(new Locale(localeAsString));
+
+            }
+        });
+    }
+
+    @SuppressLint("NewApi")
+    private void say(String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+            mTts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+
+                @Override
+                public void onDone(String utteranceId) {
+                    log("onDone: " + utteranceId);
+                }
+
+                @Override
+                public void onError(String utteranceId) {
+                    log("onError: " + utteranceId);
+                }
+
+                @Override
+                public void onStart(String utteranceId) {
+                    log("onStart: " + utteranceId);
+                }
+            });
+        } else {
+            mTts.setOnUtteranceCompletedListener(new TextToSpeech.OnUtteranceCompletedListener() {
+                @Override
+                public void onUtteranceCompleted(String utteranceId) {
+                    log("onUtteranceCompleted: " + utteranceId);
+                }
+            });
+        }
+        HashMap<String, String> params = new HashMap<>();
+        params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, UTT_COMPLETED_FEEDBACK);
+        mTts.speak(text, TextToSpeech.QUEUE_FLUSH, params);
+    }
+
+
+    /**
+     * Interrupts the current utterance and discards other utterances in the queue.
+     *
+     * @return {@ERROR} or {@SUCCESS}
+     */
+    public int stop() {
+        return mTts.stop();
+    }
+
+
+    public boolean isLanguageAvailable(String localeAsStr) {
+        return mTts.isLanguageAvailable(new Locale(localeAsStr)) >= 0;
+    }
+
+
+    public void setLanguage(Locale locale) {
+        mTts.setLanguage(locale);
+    }
+
+
+    public void shutdown() {
+        if (mTts != null) {
+            mTts.shutdown();
+        }
+    }
+
+    private void log(final String str) {
+        mTvMessages.post(new Runnable() {
+            @Override
+            public void run() {
+                mTvMessages.setText(mTvMessages.getText() + "\n" + str);
+            }
+        });
+    }
 }
--- app/src/main/java/ee/eki/heli/EKISpeak/EKISpeakService.java
+++ app/src/main/java/ee/eki/heli/EKISpeak/EKISpeakService.java
@@ -1,214 +1,210 @@
 package ee.eki.heli.EKISpeak;
 
+import android.media.AudioFormat;
+import android.speech.tts.SynthesisCallback;
+import android.speech.tts.SynthesisRequest;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeechService;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Locale;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.media.AudioFormat;
-
-import android.speech.tts.SynthesisCallback;
-import android.speech.tts.SynthesisRequest;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeechService;
-import android.util.Log;
-
-/**
- * It exercises all aspects of the Text to speech engine API
- * {@link android.speech.tts.TextToSpeechService}.
- */
 public class EKISpeakService extends TextToSpeechService {
 
-    public static final String LANG = "et";
-    public static final String COUNTRY = "EE";
+    public static final String DEFAULT_LANG = "est";
+    public static final String DEFAULT_COUNTRY = "EST";
+    public static final Locale DEFAULT_LOCALE = new Locale(DEFAULT_LANG, DEFAULT_COUNTRY);
+    public static final String[] DEFAULT_LOCALE_TRIPLE = new String[]{DEFAULT_LANG, DEFAULT_COUNTRY, ""};
 
-	private static final int SAMPLING_RATE_HZ = 48000;
+    private static final int SAMPLING_RATE_HZ = 48000;
 
-	private volatile String[] mCurrentLanguage = null;
-	private volatile boolean mStopRequested = false;
-	private SharedPreferences mSharedPrefs = null;
+    private volatile String[] mCurrentLanguage = null;
 
-	private MyCallback jniCallback = new MyCallback() ;
-	
-	@Override
-	public void onCreate() {
-		Log.w("ss", "onCreate: ");
-		super.onCreate();
-		mSharedPrefs = getSharedPreferences(
-				GeneralSettingsFragment.SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+    private volatile boolean mStopRequested = false;
 
-		onLoadLanguage(LANG, COUNTRY, "");
-	}
+    private MyCallback jniCallback = new MyCallback();
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		Log.w("ss", "onDestroy:");
-		ShutdownHTS();
-	}
 
-	@Override
-	protected String[] onGetLanguage() {
-		// Note that mCurrentLanguage is volatile because this can be called
-		// from
-		// multiple threads.
-		return mCurrentLanguage;
-	}
+    @Override
+    public void onCreate() {
+        Log.i("onCreate");
+        super.onCreate();
+        //onLoadLanguage(DEFAULT_LANG, DEFAULT_COUNTRY, "");
+    }
 
-	@Override
-	protected int onIsLanguageAvailable(String lang, String country,
-			String variant) {
-		if (LANG.equals(lang)) {
-			if (COUNTRY.equals(country)) {
-				return TextToSpeech.LANG_COUNTRY_AVAILABLE;
-			}
-			return TextToSpeech.LANG_AVAILABLE;
-		}
-        return TextToSpeech.LANG_AVAILABLE;
-		//return TextToSpeech.LANG_NOT_SUPPORTED;
-	}
+    @Override
+    public void onDestroy() {
+        Log.i("onDestroy");
+        super.onDestroy();
+        ShutdownHTS();
+    }
 
-	/*
-	 * Note that this method is synchronized, as is onSynthesizeText because
-	 * onLoadLanguage can be called from multiple threads (while
-	 * onSynthesizeText is always called from a single thread only).
-	 */
-	@Override
-	protected synchronized int onLoadLanguage(String lang, String country,
-			String variant) {
-		Log.w("ss", "onLoadLanguage: " + lang + " " + country + " " + variant);
-		final int isLanguageAvailable = onIsLanguageAvailable(lang, country,
-				variant);
+    @Override
+    protected String[] onGetLanguage() {
+        Log.i("onGetLanguage");
+        if (mCurrentLanguage == null) {
+            return DEFAULT_LOCALE_TRIPLE;
+        }
+        return mCurrentLanguage;
+    }
 
-		if (isLanguageAvailable == TextToSpeech.LANG_NOT_SUPPORTED) {
-			return isLanguageAvailable;
-		}
+    @Override
+    protected int onIsLanguageAvailable(String lang, String country, String variant) {
+        Log.i("onIsLanguageAvailable: " + lang + "/" + country + "/" + variant);
+        if (DEFAULT_LANG.equals(lang)) {
+            if (DEFAULT_COUNTRY.equals(country)) {
+                return TextToSpeech.LANG_COUNTRY_AVAILABLE;
+            }
+            return TextToSpeech.LANG_AVAILABLE;
+        }
+        return TextToSpeech.LANG_NOT_SUPPORTED;
+    }
 
-		String loadCountry = country;
-		if (isLanguageAvailable == TextToSpeech.LANG_AVAILABLE) {
-			loadCountry = "EST";
-		}
+    /*
+     * Note that this method is synchronized, as is onSynthesizeText because
+     * onLoadLanguage can be called from multiple threads (while
+     * onSynthesizeText is always called from a single thread only).
+     */
+    @Override
+    protected synchronized int onLoadLanguage(String lang, String country, String variant) {
+        Log.i("onLoadLanguage: " + lang + "/" + country + "/" + variant);
+        final int isLanguageAvailable = onIsLanguageAvailable(lang, country, variant);
 
-		// If we've already loaded the requested language, we can return early.
-		if (mCurrentLanguage != null) {
-			if (mCurrentLanguage[0].equals(lang)
-					&& mCurrentLanguage[1].equals(country)) {
-				return isLanguageAvailable;
-			}
-		}
+        if (isLanguageAvailable == TextToSpeech.LANG_NOT_SUPPORTED) {
+            return isLanguageAvailable;
+        }
 
-		Log.w("ss", "InitHTS:");
-		InitHTS(getFolderW());
+        String loadCountry = country;
+        if (isLanguageAvailable == TextToSpeech.LANG_AVAILABLE) {
+            loadCountry = DEFAULT_COUNTRY;
+        }
 
-		Log.w("ss", "InitHTS:ok");
+        // If we've already loaded the requested language, we can return early.
+        if (mCurrentLanguage != null) {
+            if (mCurrentLanguage[0].equals(lang) && mCurrentLanguage[1].equals(country)) {
+                return isLanguageAvailable;
+            }
+        }
 
-		mCurrentLanguage = new String[] { lang, loadCountry, "" };
+        Log.i("InitHTS");
 
-		return isLanguageAvailable;
-	}
+        InitHTS(getFolderW());
 
-	protected String getFolderW() {
+        Log.i("InitHTS:ok");
 
-		final int[] id = { R.raw.eki, R.raw.format, R.raw.stems };
-		final String[] name = { "eki.htsvoice", "format.gzxt", "stems.gzxt" };
-		final int totalIds = 3;
+        mCurrentLanguage = new String[]{lang, loadCountry, ""};
 
-		String dir;
-		dir = getExternalFilesDir(null) + "";
-		for (int i = 0; i < totalIds; i++) {
-			final File file = new File(dir, name[i]);
-			if (!file.exists()) {
-				InputStream input = getResources().openRawResource(id[i]);
-				try {
-					final OutputStream output = new FileOutputStream(file);
-					try {
-						try {
-							final byte[] buffer = new byte[1024];
-							int read;
+        return isLanguageAvailable;
+    }
 
-							while ((read = input.read(buffer)) != -1)
-								output.write(buffer, 0, read);
+    /**
+     * Copy files from raw to external, only if not there yet.
+     */
+    protected String getFolderW() {
 
-							output.flush();
-						} finally {
-							output.close();
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				} catch (FileNotFoundException e1) {
-					e1.printStackTrace();
-				} finally {
-					try {
-						input.close();
-					} catch (IOException e) {
-					}
-				}
-			}
+        final int[] id = {R.raw.eki, R.raw.format, R.raw.stems};
+        final String[] name = {"eki.htsvoice", "format.gzxt", "stems.gzxt"};
+        final int totalIds = 3;
 
-		}
-		return dir + "/";
-	}
+        String dir;
+        dir = getExternalFilesDir(null) + "";
+        for (int i = 0; i < totalIds; i++) {
+            final File file = new File(dir, name[i]);
+            if (!file.exists()) {
+                InputStream input = getResources().openRawResource(id[i]);
+                try {
+                    final OutputStream output = new FileOutputStream(file);
+                    try {
+                        try {
+                            final byte[] buffer = new byte[1024];
+                            int read;
 
-	@Override
-	protected void onStop() {
-		mStopRequested = true;
-		jniCallback.setStop(mStopRequested);
-	}
+                            while ((read = input.read(buffer)) != -1)
+                                output.write(buffer, 0, read);
 
-	@Override
-	protected synchronized void onSynthesizeText(SynthesisRequest request,
-			SynthesisCallback callback) {
-		Log.w("ss", "onSynthesizeText: " + request.getText());
-		int load = onLoadLanguage(request.getLanguage(), request.getCountry(),
-				request.getVariant());
+                            output.flush();
+                        } finally {
+                            output.close();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                } finally {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
 
-		if (load == TextToSpeech.LANG_NOT_SUPPORTED) {
-			callback.error();
-			return;
-		}
+        }
+        return dir + "/";
+    }
 
-		callback.start(SAMPLING_RATE_HZ, AudioFormat.ENCODING_PCM_16BIT, 1);
-		
-		
+    @Override
+    protected void onStop() {
+        Log.i("onStop");
+        mStopRequested = true;
+        jniCallback.setStop(mStopRequested);
+    }
 
-		// Someone called onStop, end the current synthesis and return.
-		if (mStopRequested) {//teisiti vaja
-			// return false;
-		}
-		Log.w("ss", "Hakka seletama: ");
-		// Get the maximum allowed size of data we can send across in 
-		// audioAvailable.
-		//final int
-		int maxBufferSize = callback.getMaxBufferSize();
-		
-		if(maxBufferSize > 16000){
-			maxBufferSize = 16000;
-		}
-		jniCallback.setCallback(callback);
-		jniCallback.setStop(false);
-		
-		SynthHTS(request.getText()+".", maxBufferSize, jniCallback);
+    @Override
+    protected synchronized void onSynthesizeText(SynthesisRequest request, SynthesisCallback callback) {
+        // TODO: map digits to words, etc.
+        String text = replaceNumbers(request.getText()) + ".";
 
-		Log.w("ss", "Seletatud: ");
-		callback.done();
+        Log.i("onSynthesizeText: " + text);
+        int load = onLoadLanguage(request.getLanguage(), request.getCountry(), request.getVariant());
 
-	}
+        if (load == TextToSpeech.LANG_NOT_SUPPORTED) {
+            callback.error();
+            return;
+        }
 
-	public static native int InitHTS(String voiseroot);
+        callback.start(SAMPLING_RATE_HZ, AudioFormat.ENCODING_PCM_16BIT, 1);
 
-	public static native int SynthHTS(String lab, int maxbufflen,
-			MyCallback callback);
+        // Someone called onStop, end the current synthesis and return.
+        if (mStopRequested) {//teisiti vaja
+            // return false;
+        }
 
-	public static native void ShutdownHTS();
+        Log.i("Start");
+        // Get the maximum allowed size of data we can send across in
+        // audioAvailable.
+        //final int
+        int maxBufferSize = callback.getMaxBufferSize();
 
-	/** Load jni .so on initialization */
-	static {
-		System.loadLibrary("ekihts");
-	}
+        if (maxBufferSize > 16000) {
+            maxBufferSize = 16000;
+        }
+        jniCallback.setCallback(callback);
+        jniCallback.setStop(false);
+
+        SynthHTS(text, maxBufferSize, jniCallback);
+
+        Log.i("Done");
+        callback.done();
+    }
+
+    private static String replaceNumbers(String str) {
+        return str.replaceAll("[0-9]+", " number ");
+    }
+
+    public static native int InitHTS(String voiseroot);
+
+    public static native int SynthHTS(String lab, int maxbufflen, MyCallback callback);
+
+    public static native void ShutdownHTS();
+
+    static {
+        System.loadLibrary("ekihts");
+    }
 
 }
